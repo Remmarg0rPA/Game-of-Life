64c6f51c86f159ab3a066d5cf59abe4018dc78d3
Remmarg0rPA <ekeberg.joel@gmail.com>
2021-06-21 19:56:30 +0200

    Fully functioning Game of Life

--- game_of_life.py
+++ game_of_life.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import random
+from os import system
+from time import sleep
 from colorama import init, Fore, Back, Style
 init()
 
@@ -20,40 +22,47 @@ def next_board_state(board):
     3. Any live cell with more than 3 live neighbors becomes dead, because of overpopulation
     4. Any dead cell with exactly 3 live neighbors becomes alive, by reproduction
     '''
-    next_board = dead_state(len(board[0]), len(board))
+    width, height = len(board[0]), len(board)
+    next_board = dead_state(width, height)
 
-    def try_board_coord(board, row,col):
+    def try_board_coord(board, row, col):
         try:
+            if row < 0 or col < 0:
+                return 0
             return board[row][col]
         except:
             return 0
 
     for row in range(len(board)):
-        for col in range(len(board[0]):
+        for col in range(len(board[0])):
             sum = 0
-            sum += try_board_coord(board, row+1, col-1)
-            sum += try_board_coord(board, row+1, col)
-            sum += try_board_coord(board, row+1, col+1)
-            sum += try_board_coord(board, row, col-1)
-            sum += try_board_coord(board, row, col)
-            sum += try_board_coord(board, row, col+1)
-            sum += try_board_coord(board, row-1, col-1)
-            sum += try_board_coord(board, row-1, col)
-            sum += try_board_coord(board, row-1, col+1)
-            if sum in (0,1):
+            sum += board[row+1][col-1] if row < height-1 and col > 0 else 0
+            sum += board[row+1][col] if row < height-1 else 0
+            sum += board[row+1][col+1] if row < height-1 and col < width-1 else 0
+            sum += board[row][col-1] if col > 0 else 0
+
+            sum += board[row][col+1] if col < width-1 else 0
+            sum += board[row-1][col-1] if row > 0 and col > 0 else 0
+            sum += board[row-1][col] if row > 0 else 0
+            sum += board[row-1][col+1] if row > 0 and col < width-1 else 0
+            if sum == 0 or sum == 1:
                 next_board[row][col] = 0
-            elif sum in (2,3):
-                next_board[row][col] = 1 if (board[row][col] or sum == 3) else 0
+            elif sum == 2:
+                next_board[row][col] = 1 if board[row][col] == 1 else 0
+            elif sum == 3:
+                next_board[row][col] = 1
             else:
                 next_board[row][col] = 0
 
     return next_board
 
 
-def render(board, *, color=False, alive = '#', dead = '-'):
+def render(board, *, color=False, alive = '#', dead = '-', erase=False):
     width = len(board[0])
     height = len(board)
 
+    if erase:
+        system('clear')
     for row in range(height):
         for col in range(width):
             c = board[row][col]
@@ -71,5 +80,10 @@ def render(board, *, color=False, alive = '#', dead = '-'):
         print()
 
 
-
-render(random_state(60,30, alive_threashold=0.8), color=True)
+if __name__ == '__main__':
+    board = random_state(70,35, alive_threashold=0.7)
+    render(board, color=True)
+    while True:
+        board = next_board_state(board)
+        render(board, color=True, erase=True)
+        sleep(0.05)
--- unit_test.py
+++ unit_test.py
@@ -5,44 +5,76 @@ from game_of_life import next_board_state
 # you move some of into reusable functions to
 # make it shorter and neater?
 
-def check_state(expexted, actual, num):
+def check_state(expected, actual, num):
     if expected == actual:
         print(f"PASSED {num}")
     else:
         print(f"FAILED {num}!")
         print("Expected:")
-        print(expected)
+        for r in expected:
+            print('\t', r)
         print("Actual:")
-        print(actual)
+        for a in actual:
+            print('\t', a)
 
 
 if __name__ == "__main__":
     # TEST 1: dead cells with no live neighbors
     # should stay dead.
-    init_state1 = [
+    init_state = [
         [0,0,0],
         [0,0,0],
         [0,0,0]
     ]
-    expected_next_state1 = [
+    expected_next_state = [
         [0,0,0],
         [0,0,0],
         [0,0,0]
     ]
-    actual_next_state1 = next_board_state(init_state1)
-    check_state(expected_next_state1, actual_next_state1, 1)
+    actual_next_state = next_board_state(init_state)
+    check_state(expected_next_state, actual_next_state, 1)
 
     # TEST 2: dead cells with exactly 3 neighbors
     # should come alive.
-    init_state2 = [
+    init_state = [
         [0,0,1],
         [0,1,1],
         [0,0,0]
     ]
-    expected_next_state2 = [
+    expected_next_state = [
         [0,1,1],
         [0,1,1],
         [0,0,0]
     ]
-    actual_next_state2 = next_board_state(init_state2)
-    check_state(expected_next_state2, actual_next_state2, 2)
+    actual_next_state = next_board_state(init_state)
+    check_state(expected_next_state, actual_next_state, 2)
+
+    # TEST 3: dead cells with exactly 1 neighbors
+    # should be dead.
+    init_state = [
+        [0,0,0],
+        [0,1,1],
+        [0,0,0]
+    ]
+    expected_next_state = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
+    actual_next_state = next_board_state(init_state)
+    check_state(expected_next_state, actual_next_state, 3)
+
+    # TEST 4: alive cells with more than 3 neighbors
+    # should be dead.
+    init_state = [
+        [1,1,1],
+        [1,1,1],
+        [1,1,1]
+    ]
+    expected_next_state = [
+        [1,0,1],
+        [0,0,0],
+        [1,0,1]
+    ]
+    actual_next_state = next_board_state(init_state)
+    check_state(expected_next_state, actual_next_state, 4)

033f8bcfd31982ce8186d68557c4ff4e3153f137
Remmarg0rPA <ekeberg.joel@gmail.com>
2021-06-22 17:37:40 +0200

    Added some small changes and README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# Conway’s Game of Life
+This is my implementation of "Conway’s Game of Life", inspired by [this tutorial](https://robertheaton.com/2018/07/20/project-2-game-of-life/)
--- game_of_life.py
+++ game_of_life.py
@@ -4,35 +4,46 @@ from colorama import init, Fore, Back, Style
 init()
 
 def dead_state(width, height):
+    '''Generates a board with all zeros, or dead cells'''
     board = [[0 for col in range(width)] for row in range(height)]
     return board
 
 
 def random_state(width, height, alive_threashold=0.5):
+    '''
+    Generates a board with cells that are randomly chosen to be dead or alive.
+    The probability of each state can be modified.
+    '''
     board = [[1 if random.random() > alive_threashold else 0 for col in range(width)] for row in range(height)]
     return board
 
 
 def next_board_state(board):
     '''
+    Generates the next board usin the following rules:
     1. Any live cell with 0 or 1 live neighbors becomes dead, because of underpopulation
     2. Any live cell with 2 or 3 live neighbors stays alive, because its neighborhood is just right
     3. Any live cell with more than 3 live neighbors becomes dead, because of overpopulation
     4. Any dead cell with exactly 3 live neighbors becomes alive, by reproduction
     '''
+
     width, height = len(board[0]), len(board)
     next_board = dead_state(width, height)
 
     def try_board_coord(board, row, col):
+        # Returning 0 does not affect result if there is no neighbour
+        # Dont want wraping around using negative index
+        if row < 0 or col < 0:
+            return 0
+        # Accessing out of range raises error instead of checking for length of board each time
         try:
-            if row < 0 or col < 0:
-                return 0
             return board[row][col]
         except:
             return 0
 
     for row in range(len(board)):
         for col in range(len(board[0])):
+            # Counting all alive neighbors in the Moore neighborhood
             sum = 0
             sum += try_board_coord(board, row+1, col-1)
             sum += try_board_coord(board, row+1, col)
@@ -43,6 +54,8 @@ def next_board_state(board):
             sum += try_board_coord(board, row-1, col-1)
             sum += try_board_coord(board, row-1, col)
             sum += try_board_coord(board, row-1, col+1)
+
+            # Implementation of the rules
             if sum == 0 or sum == 1:
                 next_board[row][col] = 0
             elif sum == 2:
@@ -56,6 +69,7 @@ def next_board_state(board):
 
 
 def render(board, *, color=False, alive = '#', dead = '-', erase=False):
+    '''Renders the board'''
     width = len(board[0])
     height = len(board)
 
@@ -82,6 +96,7 @@ def render(board, *, color=False, alive = '#', dead = '-', erase=False):
 
 
 def load_from_file(filepath):
+    '''Loads the starting board from a file.'''
     with open(filepath, 'r') as f:
         file = f.read()
 
@@ -95,21 +110,21 @@ def load_from_file(filepath):
                 buf_row.append(1)
             else:
                 buf_row.append(0)
+        # Sometimes there is an extra empty line in files, we dont want it
         if buf_row != []:
             board.append(buf_row)
 
     for i in range(1, len(board)):
-        assert len(board[i]) == len(board[i-1]), f"Error: not all lines in the file are of the same length {i}"
+        assert len(board[i]) == len(board[i-1]), f"All lines in the file must be of the same length! Error on line {i+1}"
     return board
 
 
 def save_to_file(filepath, board):
+    '''Saves the starting board to a file'''
     with open(filepath, 'w') as f:
         for row in board:
-            for c in row:
-                f.write(str(c))
-            f.write('\n' if row is not board[-1] else '')
-    return True
+            f.write(''.join(map(str, row)) + ('\n' if row is not board[-1] else ''))
+
 
 if __name__ == '__main__':
     from os import system
@@ -117,13 +132,13 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-W', '--width', type=int, help='specify width of board')
-    parser.add_argument('-H', '--height', type=int, help='specify height of board')
+    parser.add_argument('-W', '--width', type=int, help='specify width of board. Defaults to 60 if a board is not loaded from a file.')
+    parser.add_argument('-H', '--height', type=int, help='specify height of board. Defaults to 30 if a board is not loaded from a file.')
     parser.add_argument('-C', '--use-color', action='store_true', help='use color when rendering')
-    parser.add_argument('-R', '--refresh', '--clear', action='store_true', help='clear/refresh terminal between each rendering')
-    parser.add_argument('-A', '--alive-threashold', type=float, help='probability of a cell not being alive when generating the board')
-    parser.add_argument('-D', '--frame-delay', type=float, help='delay between each render. default to 0.05 seconds.')
-    parser.add_argument('-L', '--load-from-file', help='path to file to load initial board from')
+    parser.add_argument('-R', '--refresh', action='store_true', help='clear the terminal between each rendering')
+    parser.add_argument('-A', '--alive-threashold', type=float, help='probability of a cell not being alive when generating the initial board. Does not affect the state of a board lodaed from a file')
+    parser.add_argument('-D', '--frame-delay', type=float, help='delay between each render. Default to 0.05 seconds.')
+    parser.add_argument('-L', '--load-from-file', help='path to file to load initial board state from. "1" represents an alive cell and "0" a dead cell.')
     parser.add_argument('-S', '--save-initial-board', help='path to file to store initial board to. This option will overwrite the file if it already exists.')
 
 
@@ -131,7 +146,6 @@ if __name__ == '__main__':
 
     color = args.use_color
     erase = args.refresh
-    alive_threashold = args.alive_threashold if args.alive_threashold else 0.5
     delay = args.frame_delay if args.frame_delay else 0.05
 
     if args.load_from_file:
@@ -139,13 +153,14 @@ if __name__ == '__main__':
     else:
         width  = args.width if args.width else 60
         height = args.height if args.height else 30
+        alive_threashold = args.alive_threashold if args.alive_threashold else 0.5
         board = random_state(width, height, alive_threashold=alive_threashold)
 
     if args.save_initial_board:
         save_to_file(args.save_initial_board, board)
 
 
-    render(board, color=color)
+    render(board, color=color, erase=erase)
     while True:
         board = next_board_state(board)
         render(board, color=color, erase=erase)
